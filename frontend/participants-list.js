3b92f9a4f941192b6ec23839763680f54f2beb11
J-freire-97 <joao.freire97@outlook.com>
2026-03-02 01:09:39 +0000

    dockerfile

--- frontend/participants-list.js
+++
@@ -1,6 +0,0 @@
-import { renderParticipantsList } from './components_js/participantsList.js';
-
-const participants =
-  JSON.parse(localStorage.getItem('participants')) || [];
-
-renderParticipantsList(participants);
